073b169ba958301e62ab21ff79b84a0731bd3fb5
Lucas Fernando <lucasfernando@MacBook-Air-de-Lucas.local>
2025-08-05 22:49:42 -0300

    init commit

---
+++ src/studycontinousintegration3/math_soma.py
@@ -0,0 +1,12 @@
+def soma(number1, number2):
+    
+    return number1 + number2
+    
+
+def main():
+    
+    resultado = soma(10, 12)
+    print(resultado)
+
+main()
+    
---
+++ tests/math_test.py
@@ -0,0 +1,4 @@
+from src.studycontinousintegration3.math_soma import soma
+
+def test_soma():
+    assert soma(10, 12) == 22
